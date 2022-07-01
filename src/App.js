e5a54019619f34f806e1f919e72961e72d8d102f
Ammar Mheir <mrsyria99997@hotmail.com>
2022-07-01 18:42:34 -0400

    First commit. Add react-unity-webgl package to react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React  from 'react';
+import {Unity, useUnityContext } from "react-unity-webgl";
 
 function App() {
+  const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
+    loaderUrl: "Build/MetnaBuild.loader.js",
+    dataUrl: "Build/MetnaBuild.data.gz",
+    frameworkUrl: "Build/MetnaBuild.framework.js.gz",
+    codeUrl: "Build/MetnaBuild.wasm.gz",
+  });
+
+  // We'll round the loading progression to a whole number to represent the
+  // percentage of the Unity Application that has loaded.
+  const loadingPercentage = Math.round(loadingProgression * 100);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {isLoaded === false && (
+        // We'll conditionally render the loading overlay if the Unity
+        // Application is not loaded.
+        <div className="loading-overlay">
+          <p>Loading... ({loadingPercentage}%)</p>
+        </div>
+      )}
+      <Unity className="unity" unityProvider={unityProvider} />
     </div>
   );
 }
