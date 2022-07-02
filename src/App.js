90e7f2080cff35e4fc306ce05782326f3e66aadb
Ammar Mheir <mrsyria99997@hotmail.com>
2022-07-01 20:03:29 -0400

    Add images folder and background image. Add react-top-loadingbar package and <Loadingbar> tag into App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-import React  from 'react';
+import React, { useState }  from 'react';
 import {Unity, useUnityContext } from "react-unity-webgl";
+import LoadingBar from 'react-top-loading-bar'
+import background from './images/background.jpg'
 
 function App() {
   const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
-    loaderUrl: "Build/MetnaBuild.loader.js",
-    dataUrl: "Build/MetnaBuild.data.gz",
-    frameworkUrl: "Build/MetnaBuild.framework.js.gz",
-    codeUrl: "Build/MetnaBuild.wasm.gz",
+    loaderUrl: "Build/SampleBuild.loader.js",
+    dataUrl: "Build/SampleBuild.data",
+    frameworkUrl: "Build/SampleBuild.framework.js",
+    codeUrl: "Build/SampleBuild.wasm",
   });
 
   // We'll round the loading progression to a whole number to represent the
   // percentage of the Unity Application that has loaded.
   const loadingPercentage = Math.round(loadingProgression * 100);
-  
+  const [progress, setProgress] = useState(0)
+
   return (
-    <div className="container">
+    <div style={{backgroundImage: `url(${background})` }} className="container">
+
+      <LoadingBar
+        color='#f11946'
+        progress={loadingPercentage}
+        onLoaderFinished={() => setProgress(0)}
+      />
+
       {isLoaded === false && (
         // We'll conditionally render the loading overlay if the Unity
         // Application is not loaded.
@@ -24,7 +34,9 @@ function App() {
           <p>Loading... ({loadingPercentage}%)</p>
         </div>
       )}
+
       <Unity className="unity" unityProvider={unityProvider} />
+
     </div>
   );
 }
