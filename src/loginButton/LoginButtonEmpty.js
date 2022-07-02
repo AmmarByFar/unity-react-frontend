2b00a54b2a20ef53b36304e2f3aced388cd634d0
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2022-07-02 13:32:03 -0500

    add class and onClick props

--- src/loginButton/LoginButtonEmpty.js
+++ src/loginButton/LoginButtonEmpty.js
@@ -1,7 +1,21 @@
 /** Empty login button, provides the base to build others from.
  * 
+ * # Required Props
+ * 
+ * - onClick: *function*; defines the onClick behavior of the button.  
+ *    Component will still render correctly without this, a button that does nothing
+ *    is just kinda pointless.
+ * 
+ * # Optional Props
+ * 
+ * - class: *string*; Space seperated list of classes to add to this component.
+ * 
  */
 
 function LoginButtonEmpty(props) {
-    
+    return 
+        <div className={['loginButton', props.class].join(' ')} onClick={props.onClick ?? ""}>
+            
+        </div>
+    ;
 }
